29d9327595a84e5d0a4a6e34c2c18491793264ae
Cosmin Briscaru <cosmin@coremaker.io>
2019-10-22 14:33:13 +0100

    service usage added and services dependencies updated

--- main.tf
+++ main.tf
@@ -3,14 +3,21 @@ data "google_project" "project" {
   depends_on = ["google_project_service.cloudresourcemanager"]
 }
 
+resource "google_project_service" "serviceusage" {
+  service = "serviceusage.googleapis.com"
+}
+
 resource "google_project_service" "compute" {
   service = "compute.googleapis.com"
+
+  depends_on = ["google_project_service.serviceusage"]
 }
 
 resource "google_project_service" "container" {
   service = "container.googleapis.com"
 
   depends_on = [
+    "google_project_service.serviceusage",
     "google_project_service.iam",
     "google_project_service.storage_api",
     "google_project_service.compute",
@@ -21,37 +28,56 @@ resource "google_project_service" "container" {
 resource "google_project_service" "containerregistry" {
   service = "containerregistry.googleapis.com"
 
-  depends_on = ["google_project_service.storage_api"]
+  depends_on = [
+    "google_project_service.storage_api",
+    "google_project_service.serviceusage"
+    ]
 }
 
 resource "google_project_service" "storage_component" {
   service = "storage-component.googleapis.com"
+
+  depends_on = ["google_project_service.serviceusage"]
 }
 
 resource "google_project_service" "storage_api" {
   service = "storage-api.googleapis.com"
+
+  depends_on = ["google_project_service.serviceusage"]
 }
 
 resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
+
+  depends_on = ["google_project_service.serviceusage"]
 }
 
 resource "google_project_service" "logging" {
   service = "logging.googleapis.com"
+
+  depends_on = ["google_project_service.serviceusage"]
 }
 
 resource "google_project_service" "monitoring" {
   service = "monitoring.googleapis.com"
+
+  depends_on = ["google_project_service.serviceusage"]
 }
 
 resource "google_project_service" "dns" {
   service = "dns.googleapis.com"
+
+  depends_on = ["google_project_service.serviceusage"]
 }
 
 resource "google_project_service" "network" {
   service = "servicenetworking.googleapis.com"
+
+  depends_on = ["google_project_service.serviceusage"]
 }
 
 resource "google_project_service" "cloudresourcemanager" {
   service = "cloudresourcemanager.googleapis.com"
+
+  depends_on = ["google_project_service.serviceusage"]
 }
