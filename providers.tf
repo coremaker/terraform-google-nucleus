b78d5ca3507c1375b1943ab571b955bf6cdf2a33
Cosmin Briscaru <cosmin@coremaker.io>
2019-10-23 11:55:22 +0100

    kubernetes token added

--- providers.tf
+++ providers.tf
@@ -5,14 +5,14 @@ provider "google" {
 
 provider "kubernetes" {
   host = google_container_cluster.kube.endpoint
-
+  token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.kube.master_auth.0.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
     host = google_container_cluster.kube.endpoint
-
+    token = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.kube.master_auth.0.cluster_ca_certificate)
   }
 
