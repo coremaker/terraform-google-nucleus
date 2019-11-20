ec23883c626243bca671dc594c23065c09130153
Cosmin Briscaru <cosmin@coremaker.io>
2019-11-20 14:41:51 +0000

    sealed-secrets and flux update

--- sealedsecrets.tf
+++ sealedsecrets.tf
@@ -30,7 +30,7 @@ resource "helm_release" "sealed_secrets" {
 
   set {
     name = "secretName"
-    value = "sealed-secrets"
+    value = kubernetes_secret.sealed_secrets_tls.0.metadata.0.name
   }
 
   set {
@@ -44,6 +44,53 @@ resource "helm_release" "sealed_secrets" {
   }
 
   depends_on = [kubernetes_cluster_role_binding.tiller_cluster_admin]
+}
+
+resource "kubernetes_secret" "sealed_secrets_tls" {
+  count      = var.sealed_secrets_enabled ? 1 : 0
+  
+  type  = "kubernetes.io/tls"
+
+  metadata {
+    name      = "sealed-secrets-tls"
+    namespace = kubernetes_namespace.sealed_secrets.0.metadata.0.name
+    labels = {
+      "sealedsecrets.bitnami.com/sealed-secrets-key" = "active"
+    }
+  }
+
+  data = {
+    "tls.crt" = tls_self_signed_cert.sealed_secrets.0.cert_pem
+    "tls.key" = tls_private_key.sealed_secrets.0.private_key_pem
+  }
+}
+
+resource "tls_self_signed_cert" "sealed_secrets" {
+  count      = var.sealed_secrets_enabled ? 1 : 0
+
+  key_algorithm   = tls_private_key.sealed_secrets.0.algorithm
+  private_key_pem = tls_private_key.sealed_secrets.0.private_key_pem
+
+  validity_period_hours = 210240
+  early_renewal_hours = 48
+
+  allowed_uses = [
+    "encipher_only"
+  ]
+
+  dns_names = ["localhost"]
+
+  subject {
+    common_name  = "localhost"
+    organization = "Coremaker"
+  }
+}
+
+resource "tls_private_key" "sealed_secrets" {
+  count      = var.sealed_secrets_enabled ? 1 : 0
+
+  algorithm = "RSA"
+  rsa_bits  = "2048"
 }
 
 resource "kubernetes_namespace" "sealed_secrets" {
