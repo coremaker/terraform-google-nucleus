2a8c70fb1f496e1629a9d19b77e8d66702a111bc
Cosmin Briscaru <cosmin@coremaker.io>
2020-04-21 15:23:50 +0100

    upgrade helm

--- providers.tf
+++ providers.tf
@@ -14,11 +14,12 @@ provider "kubernetes" {
 provider "helm" {
   version = "v1.1.1"
 
-  # kubernetes {
-  #   host = google_container_cluster.kube.endpoint
-  #   token = data.google_client_config.default.access_token
-  #   cluster_ca_certificate = base64decode(google_container_cluster.kube.master_auth.0.cluster_ca_certificate)
-  # }
+  kubernetes {
+    load_config_file = false
+    host = google_container_cluster.kube.endpoint
+    token = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.kube.master_auth.0.cluster_ca_certificate)
+  }
 }
 
 provider "mongodbatlas" {}
