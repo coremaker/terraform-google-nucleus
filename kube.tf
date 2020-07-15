f7363074803a8d16d6a62e887f1f7d23ab58cae9
Cosmin Briscaru <cosmin@coremaker.io>
2020-07-15 15:30:20 +0100

    fix data block

--- kube.tf
+++ kube.tf
@@ -63,7 +63,7 @@ resource "google_container_node_pool" "kube_nodes" {
   name       = each.key
   cluster    = google_container_cluster.kube.name
 
-  autoscaling = {
+  autoscaling {
     min_node_count = each.value.min_node_count
     max_node_count = each.value.max_node_count
   }
