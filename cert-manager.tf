5c34190b45790792659f685efd892a3a54fbebdb
Cosmin Briscaru <cosmin@coremaker.io>
2022-01-20 15:40:38 +0000

    fix cluster name

--- cert-manager.tf
+++ cert-manager.tf
@@ -1,8 +1,8 @@
 resource "google_service_account" "cert_manager_account" {
   count      = var.cert_manager_enabled ? 1 : 0
 
-  account_id   = "${var.k8s_cluster_name}-cert-manager"
-  display_name = "Cert manager service accountfor the ${var.k8s_cluster_name} kube cluster"
+  account_id   = "${var.gke_cluster_name}-cert-manager"
+  display_name = "Cert manager service accountfor the ${var.gke_cluster_name} kube cluster"
 
   depends_on = [google_project_service.iam]
 }
