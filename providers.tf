6693afa9180d370a3b9ea26f9d8f6682a0d51e2e
Cosmin Briscaru <cosmin@coremaker.io>
2022-01-20 15:11:32 +0000

    remove google provider

--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.0.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     google = {
+#       source = "hashicorp/google"
+#       version = "4.0.0"
+#     }
+#   }
+# }
 
-provider "google" {
-  project = var.google_project_id
-  region = var.google_region
-}
+# provider "google" {
+#   project = var.google_project_id
+#   region = var.google_region
+# }
