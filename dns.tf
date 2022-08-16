db6ff7cde246b651e604a663ce8a68fd77fbd53d
Cosmin Briscaru <cosmin@coremaker.io>
2022-08-16 15:32:30 +0100

    remove brackets

--- dns.tf
+++ dns.tf
@@ -21,7 +21,7 @@ resource "google_dns_record_set" "dns_record" {
   type         = "A"
   ttl          = 1800
   managed_zone = google_dns_managed_zone.dns_zone.0.name
-  rrdatas      = each.value.regional_ip ? [google_compute_address.namespace_regional_public_ip[each.value].address] : [google_compute_global_address.namespace_public_ip[each.value].address]
+  rrdatas      = each.value.regional_ip ? google_compute_address.namespace_regional_public_ip[each.value].address : google_compute_global_address.namespace_public_ip[each.value].address
 
   depends_on = [google_dns_managed_zone.dns_zone]
 }
