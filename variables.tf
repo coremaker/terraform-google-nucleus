2e09666030c12fde483cc2a7d2f7a5719ba7e0a9
alexandra-buica <109084033+alexandra-buica@users.noreply.github.com>
2025-04-11 11:35:25 +0300

    set linux node config as optional

--- variables.tf
+++ variables.tf
@@ -84,9 +84,9 @@ variable "gke_node_pools" {
     disk_size_gb   = optional(string, "100")
     disk_type      = optional(string, "pd-ssd")
     spot           = optional(string, "false")
-    linux_node_config = object({
+    linux_node_config = optional(object({
       cgroup_mode = optional(string)
-    })
+    }))
     node_locations               = optional(list(string))
     node_additional_oauth_scopes = optional(list(string), [])
     taints = optional(list(object({
